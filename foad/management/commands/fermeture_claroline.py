286b9a6b301c2be2eea2d1c49a1360f4995f3c92
paul <paul.guichon@iedparis8.net>
2014-10-14 17:20:25 +0200

    git pull

--- foad/management/commands/fermeture_claroline.py
+++ foad/management/commands/fermeture_claroline.py
@@ -6,7 +6,7 @@ from django.core.management.base import BaseCommand
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
-        etape = ["L3NEDU",]
+        etape = ["L3NEDU", ]
         # "L2NINF", "L1NINF", "L3NDRO", "L2NDRO", "L1NDRO", "L3NEDU", "M2NEFI",
         #          "M1NEFI", "L3NPSY", "L2NPSY", "L1NPSY", "M2NPCL", "M1NPCL", "M2NPST", "M1NPST",
         #          "M2NPEA", "M1NPEA"]
