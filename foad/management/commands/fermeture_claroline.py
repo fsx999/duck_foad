dc3a315b646148e2e9f27d886c1dabac4ee23a1c
paul <paul.guichon@iedparis8.net>
2014-10-15 18:09:27 +0200

    git pull

--- foad/management/commands/fermeture_claroline.py
+++ foad/management/commands/fermeture_claroline.py
@@ -12,12 +12,12 @@ class Command(BaseCommand):
                  "M2NPEA", "M1NPEA"]
         etape += ["DSNATA"]
 
-        cours = list(FoadCour.objects.using('foad').filter(faculte__in=etape).values_list('code', flat=True))
+        cours = list(FoadCour.objects.using('foad_test').filter(faculte__in=etape).values_list('code', flat=True))
         cours_user = FoadCourUser.objects.using('foad_test').exclude(statut__in=[3,1,2], user__statut__in=[3,1,2])
         users = [x[0] for x in cours_user.values_list('user')]
         dips = FoadDip.objects.using('foad').filter(user__in=users)
-        cours_user.using('foad').delete()
-        dips.using('foad').delete()
+        cours_user.using('foad_test').delete()
+        dips.using('foad_test').delete()
         print cours_user.count()
         print dips.count()
 
