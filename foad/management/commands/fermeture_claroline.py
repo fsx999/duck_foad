83fbec03a137d88ad33a8e14eb5cd60e6e825d2b
paul <paul.guichon@iedparis8.net>
2014-10-15 18:09:02 +0200

    git pull

--- foad/management/commands/fermeture_claroline.py
+++ foad/management/commands/fermeture_claroline.py
@@ -6,16 +6,16 @@ from django.core.management.base import BaseCommand
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
-        etape = ["L3NEDU", ]
-        # "L2NINF", "L1NINF", "L3NDRO", "L2NDRO", "L1NDRO", "L3NEDU", "M2NEFI",
-        #          "M1NEFI", "L3NPSY", "L2NPSY", "L1NPSY", "M2NPCL", "M1NPCL", "M2NPST", "M1NPST",
-        #          "M2NPEA", "M1NPEA"]
-        #etape += ["DSNATA"]
+        etape = ["L3NEDU",
+        "L2NINF", "L1NINF", "L3NDRO", "L2NDRO", "L1NDRO", "L3NEDU", "M2NEFI",
+                 "M1NEFI", "L3NPSY", "L2NPSY", "L1NPSY", "M2NPCL", "M1NPCL", "M2NPST", "M1NPST",
+                 "M2NPEA", "M1NPEA"]
+        etape += ["DSNATA"]
 
         cours = list(FoadCour.objects.using('foad').filter(faculte__in=etape).values_list('code', flat=True))
-        cours_user = FoadCourUser.objects.using('foad').filter(code_cours__in=cours, user__statut=5)
+        cours_user = FoadCourUser.objects.using('foad_test').exclude(statut__in=[3,1,2], user__statut__in=[3,1,2])
         users = [x[0] for x in cours_user.values_list('user')]
-        dips = FoadDip.objects.using('foad').filter(user__in=users, user__statut=5)
+        dips = FoadDip.objects.using('foad').filter(user__in=users)
         cours_user.using('foad').delete()
         dips.using('foad').delete()
         print cours_user.count()
