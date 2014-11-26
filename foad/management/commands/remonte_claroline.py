7af39d08bc387c77889a4789200ee5352735a392
paul <paul.guichon@iedparis8.net>
2014-11-26 15:14:44 +0100

    ajout ip

--- foad/management/commands/remonte_claroline.py
+++ foad/management/commands/remonte_claroline.py
@@ -5,7 +5,9 @@ from mailrobot.models import Mail
 from datetime import date, datetime
 from django.core.mail import send_mail
 from django_apogee.models import InsAdmEtp
-from foad.models import FoadUser, FoadDip, FoadCour, FoadCourUser, CompteMail, SettingsEtapeFoad, Remontee
+from django_apogee.utils import make_etudiant_password
+from foad.models import FoadUser, FoadDip, FoadCour, FoadCourUser, CompteMail, SettingsEtapeFoad, Remontee, \
+    AuditeurLibreApogee
 from django.core.management.base import BaseCommand
 import os
 from foad.utils import remontee_claroline
@@ -22,26 +24,40 @@ class Command(BaseCommand):
             COURS[e] = [x[0] for x in FoadCour.objects.using('foad').filter(faculte=e).values_list('code')]
         cp = 0
         message = u"la remonté dans claroline s'est effectuée\n"
-        erreur = False
-        for x in InsAdmEtp.inscrits.filter(remontee__isnull=True):
-            Remontee.objects.create(etape=x)
-        for etape in etapes:
-            etps = list(etape.mptt.get_descendants(include_self=True).values_list('etape__cod_etp', flat=True))
-            c2i = etape.c2i
-            cod_etp = etape.cod_etp
-            for inscription in InsAdmEtp.inscrits.filter(cod_etp=cod_etp, remontee__remontee=False):
-                try:
-                    cp += remontee_claroline(inscription, etps, c2i, 'foad', COURS, mail=mail)
-                    if not cp % 100:
-                        time.sleep(2)
-                except FoadDip.MultipleObjectsReturned:
-                    message += u"multi FoadDip %s %s\n" % (inscription.cod_ind.cod_etu, inscription.cod_etp)
-                except IntegrityError:
-                    message += u"Integrity error %s %s\n" % (inscription.cod_etp, inscription.cod_ind.cod_etu)
-                except UnicodeEncodeError:
-                    message += u"Unicode erreur %s\n" % inscription.cod_ind.cod_etu
-                except Exception, e:
-                    message += u"erreur %s \n" % e
+        # for x in InsAdmEtp.inscrits.filter(remontee__isnull=True):
+        #     Remontee.objects.create(etape=x)
+        # for etape in etapes:
+        #     etps = list(etape.mptt.get_descendants(include_self=True).values_list('etape__cod_etp', flat=True))
+        #     c2i = etape.c2i
+        #     cod_etp = etape.cod_etp
+        #     for inscription in InsAdmEtp.inscrits.filter(cod_etp=cod_etp, remontee__remontee=False):
+        #         try:
+        #             cp += remontee_claroline(inscription, etps, c2i, 'foad', COURS, mail=mail)
+        #             if not cp % 100:
+        #                 time.sleep(2)
+        #         except FoadDip.MultipleObjectsReturned:
+        #             message += u"multi FoadDip %s %s\n" % (inscription.cod_ind.cod_etu, inscription.cod_etp)
+        #         except IntegrityError:
+        #             message += u"Integrity error %s %s\n" % (inscription.cod_etp, inscription.cod_ind.cod_etu)
+        #         except UnicodeEncodeError:
+        #             message += u"Unicode erreur %s\n" % inscription.cod_ind.cod_etu
+        #         except Exception, e:
+        #             message += u"erreur %s \n" % e
+        for auditeur in AuditeurLibreApogee.objects.filter(status_modified=True):
+            auditeur.cod_etp = 'L1NPSY'
+            auditeur.cod_ind = auditeur
+            auditeur.cod_etu = auditeur.code_ied
+            remontee_claroline(auditeur, ['L1NPSY'], False, function=save_auditeur)
         message += u"il y a eu %s mail envoyé" % cp
         send_mail("remontee claroline", message, 'nepasrepondre@iedparis8.net', ['paul.guichon@iedparis8.net'])
         print "fini"
+
+
+def save_auditeur(auditeur, user_foad, db):
+    user_foad.email = auditeur.code_ied + '@foad.iedparis8.net'
+    user_foad.nom = auditeur.last_name
+    user_foad.prenom = auditeur.first_name
+    user_foad.statut = 5
+    user_foad.official_code = auditeur.code_ied
+    user_foad.password = make_etudiant_password(auditeur.code_ied[:-1])
+    user_foad.save(using=db)  # création de l'user
