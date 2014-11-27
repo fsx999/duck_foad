a6b59e931527aec9cb360548ef22626d1caf5331
paul <paul.guichon@iedparis8.net>
2014-11-27 10:19:06 +0100

    ajout ip

--- foad/management/commands/remonte_claroline.py
+++ foad/management/commands/remonte_claroline.py
@@ -24,25 +24,25 @@ class Command(BaseCommand):
             COURS[e] = [x[0] for x in FoadCour.objects.using('foad').filter(faculte=e).values_list('code')]
         cp = 0
         message = u"la remonté dans claroline s'est effectuée\n"
-        # for x in InsAdmEtp.inscrits.filter(remontee__isnull=True):
-        #     Remontee.objects.create(etape=x)
-        # for etape in etapes:
-        #     etps = list(etape.mptt.get_descendants(include_self=True).values_list('etape__cod_etp', flat=True))
-        #     c2i = etape.c2i
-        #     cod_etp = etape.cod_etp
-        #     for inscription in InsAdmEtp.inscrits.filter(cod_etp=cod_etp, remontee__remontee=False):
-        #         try:
-        #             cp += remontee_claroline(inscription, etps, c2i, 'foad', COURS, mail=mail)
-        #             if not cp % 100:
-        #                 time.sleep(2)
-        #         except FoadDip.MultipleObjectsReturned:
-        #             message += u"multi FoadDip %s %s\n" % (inscription.cod_ind.cod_etu, inscription.cod_etp)
-        #         except IntegrityError:
-        #             message += u"Integrity error %s %s\n" % (inscription.cod_etp, inscription.cod_ind.cod_etu)
-        #         except UnicodeEncodeError:
-        #             message += u"Unicode erreur %s\n" % inscription.cod_ind.cod_etu
-        #         except Exception, e:
-        #             message += u"erreur %s \n" % e
+        for x in InsAdmEtp.inscrits.filter(remontee__isnull=True):
+            Remontee.objects.create(etape=x)
+        for etape in etapes:
+            etps = list(etape.mptt.get_descendants(include_self=True).values_list('etape__cod_etp', flat=True))
+            c2i = etape.c2i
+            cod_etp = etape.cod_etp
+            for inscription in InsAdmEtp.inscrits.filter(cod_etp=cod_etp, remontee__remontee=False):
+                try:
+                    cp += remontee_claroline(inscription, etps, c2i, 'foad', COURS, mail=mail)
+                    if not cp % 100:
+                        time.sleep(2)
+                except FoadDip.MultipleObjectsReturned:
+                    message += u"multi FoadDip %s %s\n" % (inscription.cod_ind.cod_etu, inscription.cod_etp)
+                except IntegrityError:
+                    message += u"Integrity error %s %s\n" % (inscription.cod_etp, inscription.cod_ind.cod_etu)
+                except UnicodeEncodeError:
+                    message += u"Unicode erreur %s\n" % inscription.cod_ind.cod_etu
+                except Exception, e:
+                    message += u"erreur %s \n" % e
         for auditeur in AuditeurLibreApogee.objects.filter(status_modified=True):
             auditeur.cod_etp = 'L1NPSY'
             auditeur.cod_ind = auditeur
