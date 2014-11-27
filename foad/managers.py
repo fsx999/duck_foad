6c4769a97f96e6e870aefc38751a3057d0c9a56f
paul <paul.guichon@iedparis8.net>
2014-11-27 13:16:13 +0100

    ajout ip

--- foad/managers.py
+++ foad/managers.py
@@ -5,7 +5,7 @@ from django.db import models
 
 class AuditeurManager(models.Manager):
 
-    def create_auditeur(self, individu):
+    def create_auditeur(self, individu, annee=2014):
         return self.get_or_create(
             last_name=individu.last_name,
             first_name=individu.first_name1,
@@ -14,5 +14,5 @@ class AuditeurManager(models.Manager):
             phone_number=individu.adresses.get(type=1).listed_number,
             code_ied=str(individu.code_opi) + 'B',
             birthday=individu.birthday,
-            annee_id=individu.annee
+            annee_id=annee
         )
