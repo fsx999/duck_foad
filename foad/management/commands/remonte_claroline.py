c119babcdc018448025c8f5fb612b85c4586a722
paul <paul.guichon@iedparis8.net>
2014-12-03 15:35:34 +0100

    ajout ip

--- foad/management/commands/remonte_claroline.py
+++ foad/management/commands/remonte_claroline.py
@@ -31,30 +31,30 @@ class Command(BaseCommand):
             c2i = etape.c2i
             cod_etp = etape.cod_etp
             # for inscription in InsAdmEtp.inscrits.filter(cod_etp=cod_etp, remontee__remontee=False):
-            for inscription in InsAdmEtp.inscrits.all():
-                try:
-                    cp += remontee_claroline(inscription, etps, c2i, 'foad', COURS, mail=mail)
-                    if not cp % 100:
-                        time.sleep(2)
-                except FoadDip.MultipleObjectsReturned:
-                    message += u"multi FoadDip %s %s\n" % (inscription.cod_ind.cod_etu, inscription.cod_etp)
-                except IntegrityError:
-                    message += u"Integrity error %s %s\n" % (inscription.cod_etp, inscription.cod_ind.cod_etu)
-                except UnicodeEncodeError:
-                    message += u"Unicode erreur %s\n" % inscription.cod_ind.cod_etu
-                except Exception, e:
-                    message += u"erreur %s \n" % e
-                print message
+            # # for inscription in InsAdmEtp.inscrits.all():
+            #     try:
+            #         cp += remontee_claroline(inscription, etps, c2i, 'foad', COURS, mail=mail)
+            #         if not cp % 100:
+            #             time.sleep(2)
+            #     except FoadDip.MultipleObjectsReturned:
+            #         message += u"multi FoadDip %s %s\n" % (inscription.cod_ind.cod_etu, inscription.cod_etp)
+            #     except IntegrityError:
+            #         message += u"Integrity error %s %s\n" % (inscription.cod_etp, inscription.cod_ind.cod_etu)
+            #     except UnicodeEncodeError:
+            #         message += u"Unicode erreur %s\n" % inscription.cod_ind.cod_etu
+            #     except Exception, e:
+            #         message += u"erreur %s \n" % e
+            #     print message
 
         # for auditeur in AuditeurLibreApogee.objects.filter(status_modified=True):
-        # for auditeur in AuditeurLibreApogee.objects.all():
-        #     auditeur.cod_etp = 'L1NPSY'
-        #     auditeur.cod_ind = auditeur
-        #     auditeur.cod_etu = auditeur.code_ied
-        #     auditeur.cod_anu = 2014
-        #     cp += remontee_claroline(auditeur, ['L1NPSY'], False, auditeur=True)
-        #     auditeur.status_modified = False
-        #     auditeur.save()
+        for auditeur in AuditeurLibreApogee.objects.all():
+            auditeur.cod_etp = 'L1NPSY'
+            auditeur.cod_ind = auditeur
+            auditeur.cod_etu = auditeur.code_ied
+            auditeur.cod_anu = 2014
+            cp += remontee_claroline(auditeur, ['L1NPSY'], False, auditeur=True)
+            auditeur.status_modified = False
+            auditeur.save()
         message += u"il y a eu %s mail envoyé" % cp
         send_mail("remontee claroline", message, 'nepasrepondre@iedparis8.net', ['paul.guichon@iedparis8.net'])
         print "fini"
