a4791e41f3fdb1cc400c951012853be96d63d1ab
paul guichon <paul.guichon@iedparis8.net>
2015-10-29 17:29:21 +0100

    filtre par 510

--- foad/management/commands/boite_mail.py
+++ foad/management/commands/boite_mail.py
@@ -11,7 +11,22 @@ from django.core.mail import send_mail
 class Command(BaseCommand):
     def handle(self, *args, **options):
         mail = Mail.objects.get(name='remontee')
-        cod_etps = ['L3NEDU', 'M1NEFI', 'L1NDRO', 'L2NDRO', 'L3NDRO']
+        cod_etps = [
+           'L3NEDU', 'M1NEFI', 'L1NDRO', 'L2NDRO', 'L3NDRO',
+            'L1NPSY',
+        'L2NPSY',
+        'L3NPSY',
+        'L1NINF',
+        'L2NINF',
+        'L3NINF',
+        'DSNATA',
+        'M1NPCL',
+        'M2NPCL',
+        'M1NPEA',
+        'M2NPEA',
+        'M1NPST',
+        'M2NPST',
+        ]
         for code_etud in InsAdmEtpInitial.inscrits.using('oracle').filter(cod_etp__in=cod_etps, cod_anu=2015):
             individu = code_etud.cod_ind
             cod = individu.lib_pr1_ind.replace(" ", "\\ ").replace("'", "\\'").replace("`", "") + '-' + individu.lib_nom_pat_ind.replace(" ", "\\ ").replace("'", "\\'").replace('(', '').replace(')', '').replace("`", "")
@@ -30,18 +45,18 @@ class Command(BaseCommand):
                     )
 
                     os.system(command)
-            context = {
-                    'etape': str(code_etud.cod_etp),
-                    'prenom': str(individu.lib_pr1_ind),
-                    'username': str(individu.cod_etu),
-                    'password': str(password),
-                    'email': str(email)
-                }
+                context = {
+                        'etape': str(code_etud.cod_etp),
+                        'prenom': str(individu.lib_pr1_ind),
+                        'username': str(individu.cod_etu),
+                        'password': str(password),
+                        'email': str(email)
+                    }
 
-            r = mail.make_message(
-                sender='nepasrepondre@iedparis8.net',
-                recipients=(individu.get_email(2015), email),
-                context=context
-            )
-            r.send()
+                r = mail.make_message(
+                    sender='nepasrepondre@iedparis8.net',
+                    recipients=(individu.get_email(2015), email),
+                    context=context
+                )
+                r.send()
         print "fini"
